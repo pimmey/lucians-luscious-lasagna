c710c79d3ecb7f48f53ca44ec7022f3ced9f8729
Yegor Borisenco <pimmey@pimmey.com>
2022-12-24 23:05:17 +0800

    feat: lasagna

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+// This stub file contains items that aren't used yet; feel free to remove this module attribute
+// to enable stricter warnings.
+// #![allow(unused)]
+
+pub fn expected_minutes_in_oven() -> i32 {
+    40
+}
+
+pub fn remaining_minutes_in_oven(actual_minutes_in_oven: i32) -> i32 {
+    expected_minutes_in_oven() - actual_minutes_in_oven
+}
+
+const LASAGNA_LAYER_PREP_TIME: i32 = 2;
+
+pub fn preparation_time_in_minutes(number_of_layers: i32) -> i32 {
+    number_of_layers * LASAGNA_LAYER_PREP_TIME
+}
+
+pub fn elapsed_time_in_minutes(number_of_layers: i32, actual_minutes_in_oven: i32) -> i32 {
+    actual_minutes_in_oven + preparation_time_in_minutes(number_of_layers)
+}
